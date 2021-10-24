5d1b3a02660f308f5b5cd69593d99a410db0e5be
jess-repos <nine.nfire@gmail.com>
2021-10-24 02:06:25 +0100

    commented force sync for sequelize

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/auth", require("./routes/auth"));
 const run = async () => {
   try {
     await connection.sync();
-    // await connection.sync({ force: true }); // wipes all table data user when make changes to models
+    // await connection.sync({ force: true }); // wipes all table data
     console.log("[DATABASE] Ready");
     await app.listen(port);
     console.log("[SERVER] Listening on port,", port);
